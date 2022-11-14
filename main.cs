7c747bcf56752349ec04811fef4a800d35afe3c5
c23TaylorMetzger <c23taylor.metzger@afacademy.af.edu>
2022-11-13 23:48:21 -0700

    Completed through 10:00 of DeFreitas video

--- main.cs
+++ main.cs
@@ -138,6 +138,9 @@ class TestParser
             Parser p = new Parser(l);
             Start s = p.Parse();
 
+            SemanticAnalyzer sa = new SemanticAnalyzer();
+            s.Apply(sa);
+
             TextPrinter printer = new TextPrinter();
             if (args.Length > 0 && args[0] == "-ansi")
                 printer.SetColor(true);

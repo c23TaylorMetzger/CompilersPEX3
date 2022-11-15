ba4081623c5943a031846aa4e9a95a267cb1c666
c23TaylorMetzger <c23taylor.metzger@afacademy.af.edu>
2022-11-14 22:53:03 -0700

    Added all Definitions to Definition Class

---
+++ Definition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS426.analysis
+{
+    public abstract class Definition
+    {
+
+        public string name;
+
+        public string toString()
+        {
+            return name;
+        }
+    }
+
+    public abstract class TypeDefinition : Definition { }
+
+    public class NumberDefinition : TypeDefinition { }
+
+    public class StringDefinition : TypeDefinition { }
+
+    public class BooleanDefinition : Definition { }
+
+    public class VariableDefinition : Definition
+    {
+        public TypeDefinition variableType;
+    }
+
+    public class FunctionDefinition : Definition
+    {
+        public List<VariableDefinition> parameters;
+    }
+}
